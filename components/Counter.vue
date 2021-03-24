a19179631bc71123728ae39d79be5cbcaf05ebc4
knagara <nagara.keita@gmail.com>
2021-03-24 15:24:45 +0900

    init

---
+++ components/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    counter: {{ $store.state.counter.counter }}
+    <button @click="$store.commit('counter/countUp')">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Counter App",
+    };
+  },
+};
+</script>
